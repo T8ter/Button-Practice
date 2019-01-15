277291aa94b48745948a7ae2a44f26ad8b6219c6
Ryan Dikeos and Tate Easterbrooks <tate@>
2019-01-15 15:36:26 -0800

    add tons of content to styling, java, and html.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,120 @@
 $(document).ready(function() {
-  $("button#green").click(function() {
-    $("body").removeClass();
-    $("body").addClass("green-background");
-  });
-
-  $("button#yellow").click(function() {
-    $("body").removeClass();
-    $("body").addClass("yellow-background");
-  });
-
-  $("button#red").click(function() {
-    $("body").removeClass();
-    $("body").addClass("red-background");
+  $(".btn-2").click(function() {
+    $(".op-hide").toggle();
   });
 });
+$(document).ready(function() {
+  $(".btn-3").click(function() {
+    $(".op-hide2").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-1").click(function() {
+    $(".op-hide3").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-4").click(function() {
+    $(".op-hide4").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-5").click(function() {
+    $(".op-hide5").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-6").click(function() {
+    $(".op-hide6").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-7").click(function() {
+    $(".op-hide7").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-8").click(function() {
+    $(".op-hide8").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-9").click(function() {
+    $(".op-hide9").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-10").click(function() {
+    $(".op-hide10").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-11").click(function() {
+    $(".op-hide11").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-12").click(function() {
+    $(".op-hide12").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-13").click(function() {
+    $(".op-hide13").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-14").click(function() {
+    $(".op-hide14").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-15").click(function() {
+    $(".op-hide15").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-16").click(function() {
+    $(".op-hide16").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-17").click(function() {
+    $(".op-hide17").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-18").click(function() {
+    $(".op-hide18").toggle();
+  });
+});
+$(document).ready(function() {
+  $(".btn-19").click(function() {
+    $(".op-hide19").toggle();
+  });
+});
+// $(document).ready(function() {
+//   $(".btn-2").click(function() {
+//     $(".op-hide").toggle();
+//   });
+// });
+// $(document).ready(function() {
+//   $(".btn-2").click(function() {
+//     $(".op-hide").toggle();
+//   });
+// });
+// $(document).ready(function() {
+//   $(".btn-2").click(function() {
+//     $(".op-hide").toggle();
+//   });
+// });
+// $(document).ready(function() {
+//   $(".btn-2").click(function() {
+//     $(".op-hide").toggle();
+//   });
+// });
+// $(document).ready(function() {
+//   $(".btn-2").click(function() {
+//     $(".op-hide").toggle();
+//   });
+// });
